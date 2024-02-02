a553793b9f3b707889a84a2242ab39535a875f74
Zach Donnelly <zachary.donnelly@walmart.com>
2024-02-02 00:46:43 -0500

    chore: gtag base config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -12,7 +12,6 @@ const config: GatsbyConfig = {
   plugins: [
     'gatsby-plugin-netlify-cms',
     'gatsby-plugin-sass',
-    'gatsby-plugin-google-gtag',
     'gatsby-plugin-image',
     'gatsby-plugin-sitemap',
     {
@@ -39,6 +38,22 @@ const config: GatsbyConfig = {
         path: './src/pages/'
       },
       __key: 'pages'
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'GA-TRACKING_ID' // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: 'OPT_CONTAINER_ID',
+          anonymize_ip: true,
+          cookie_expires: 0
+        }
+      }
     }
   ]
 }
